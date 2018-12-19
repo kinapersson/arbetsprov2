9ebe651aab27bf20d677ef52f0e6628cd949f957
kinapersson <persson.kina@gmail.com>
2018-12-19 13:32:31 +0100

    added comments and css clean up

--- scripts.js
+++ scripts.js
@@ -1,23 +1,26 @@
 var h1 = document.getElementById("greeting");
 var form = document.getElementById("form");
 
+//Checking if localstorage has any data and adds default users
 var names = localStorage.getItem('names');
 names = names ? JSON.parse(names) : ['Hejpa', 'Dejpa'];
 
 function addName(name) { 
 
     form.classList.add("hidden");
-    h1.classList.add("ascend");
+    h1.classList.add("changed");
 
     if (names.includes(name)) {   
         h1.innerHTML = ("Välkommen tillbaka " + name + "!");
     } else {
+        //Adds new user to LS
         names.push(name);
         localStorage.setItem('names', JSON.stringify(names));
         h1.innerHTML = ("Välkommen "  + name + "!");
     }
 }
 
+//Gets value from DOM
 function submitForm() {
   addName(document.getElementById('text').value)
   return false;
